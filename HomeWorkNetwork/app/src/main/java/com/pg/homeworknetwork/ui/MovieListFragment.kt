b80f7f64ac1975745a75e29fbdfa978beec61711
Alexander Tarabrin <aleksandr.tarabrin@rt.ru>
2022-04-06 09:32:13 +0300

    Network Complete RxJava3 Example

--- HomeWorkNetwork/app/src/main/java/com/pg/homeworknetwork/ui/MovieListFragment.kt
+++ HomeWorkNetwork/app/src/main/java/com/pg/homeworknetwork/ui/MovieListFragment.kt
@@ -1,15 +1,19 @@
 package com.pg.homeworknetwork.ui
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.pg.homeworknetwork.data.Client
 import com.pg.homeworknetwork.data.Movies
 import com.pg.homeworknetwork.R
+import com.pg.homeworknetwork.ui.recycler.EndlessScrollListener
 import com.pg.homeworknetwork.ui.recycler.MovieItemAdapter
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.schedulers.Schedulers
@@ -18,10 +22,11 @@ class MovieListFragment : Fragment(R.layout.fragment_movie_list) {
 
     private lateinit var loading: View
     private lateinit var recycler: RecyclerView
+    private lateinit var swipeRefreshLayout: SwipeRefreshLayout
+    private lateinit var endlessScroll: EndlessScrollListener
 
-//    private lateinit var adapter: MovieItemAdapter
-//    private lateinit var endlessScroll: EndlessScrollListener
-//    private val movies = mutableListOf<Movies.Short>()
+    private lateinit var adapter: MovieItemAdapter
+    private val movies = mutableListOf<Movies.Short>()
 
     private val goToDetails = object : MovieItemAdapter.IOnItemClick {
         override fun onItemClick(movie: Movies.Short) {
@@ -38,31 +43,54 @@ class MovieListFragment : Fragment(R.layout.fragment_movie_list) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         loading = view.findViewById(R.id.loading)
+        swipeRefreshLayout = view.findViewById(R.id.swipe_refresh)
+        swipeRefreshLayout.setOnRefreshListener {
+            endlessScroll.resetState()
+            load()
+        }
 
         recycler = view.findViewById<RecyclerView>(R.id.recycler).apply {
             setHasFixedSize(true)
-            layoutManager = LinearLayoutManager(context)
-            adapter = MovieItemAdapter().apply { setClickListener(goToDetails) }
+            layoutManager = LinearLayoutManager(context).also { layoutManager ->
+                addOnScrollListener(object : EndlessScrollListener(layoutManager) {
+                    override fun onLoadMore(page: Int, totalItemsCount: Int, view: RecyclerView?) {
+                        load(page + 1)
+                    }
+                }.also { endlessScroll = it })
+            }
+            adapter = MovieItemAdapter()
+                .apply { setClickListener(goToDetails) }
+                .also { this@MovieListFragment.adapter = it }
         }
 
         load()
-
         super.onViewCreated(view, savedInstanceState)
     }
 
-    private fun load() {
-        Client.popular()
+    private fun load(page: Int = FIRST_PAGE) {
+        Client.popular(page)
             .subscribeOn(Schedulers.io())
             .map(Movies::moviesShort)
             .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(::onListLoaded, Throwable::printStackTrace)
+            .doOnSubscribe { loading.visibility = View.VISIBLE }
+            .doOnTerminate { loading.visibility = View.INVISIBLE }
+            .subscribe(
+                { pageContents -> onListLoaded(page, pageContents) },
+                Throwable::printStackTrace
+            )
     }
 
-    private fun onListLoaded(list: List<Movies.Short>) {
-        (recycler.adapter as MovieItemAdapter).submitList(list)
+    private fun onListLoaded(page: Int, list: List<Movies.Short>) {
+        swipeRefreshLayout.isRefreshing = false
+        if (page == FIRST_PAGE) {
+            movies.clear()
+        }
+        movies.addAll(list)
+        adapter.submitList(movies)
     }
 
     companion object {
         const val TAG = "MovieListFragment"
+        const val FIRST_PAGE = 1
     }
 }
